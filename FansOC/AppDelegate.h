dab868c37daf68e25cb2abbb8f2bb801aa753bb5
fzw <fengzhiwen.59@bytedance.com>
2023-06-06 16:18:41 +0800

    初始化项目

---
+++ FansOC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FansOC
+//
+//  Created by ByteDance on 2023/6/6.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
